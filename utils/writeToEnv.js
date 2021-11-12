ed26a5ed85407b6803aee85fd9f1d9ea23f27805
Jordan Whistler <whistler.5@osu.edu>
2021-11-12 12:01:44 -0500

    x

--- utils/writeToEnv.js
+++ utils/writeToEnv.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 require("dotenv").config({ path: `${configDir}/.env` });
 
 // Appends a new ENV pair to the end of the .env file.  Does not currently replace the existing value of a pre-existing entry.
-const writeToEnv = (name, val) => {
+function writeToEnv(name, val) {
   process.env[name] = val;
   fs.appendFileSync(`${configDir}/.env`, `\n${name}=${val}`);
-};
+}
 
 module.exports = writeToEnv;
