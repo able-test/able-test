a13ec85d1551abb7223ed325560f4d500342ccbb
Jenae Janzen <63259035+Jjanzen27@users.noreply.github.com>
2021-11-17 15:45:33 -0500

    Add error handling to update command for missing ableConfig file

--- commands/update.js
+++ commands/update.js
@@ -1,8 +1,22 @@
-const createRemoteConfig = require("../actions/createRemoteConfig");
 const log = require("../utils/log");
 const prompt = require("prompts");
 const loadingBar = require("../utils/loadingBar");
-const loadAbleConfig = require("../utils/loadAbleConfig")
+const fs = require("fs");
+let createRemoteConfig;
+
+const ableConfigExists = () => {
+  return fs.existsSync(`${process.cwd()}/ableConfig.json`)
+}
+
+const loadAbleConfig = () => {
+  createRemoteConfig = require("../actions/createRemoteConfig.js")
+}
+
+const missingAbleConfigMessage = () => {
+  log("\n`ableConfig.json` not found. Please run `able config` to create file.")
+  log("\nBefore deploying your Able test, edit your `ableConfig` to configure your tests.")
+  log("\nFor more information about test configuration, run `able config --help`.\n")
+}
 
 const update = async () => {
   const update = loadingBar("\n*Updating remote test configuration*\n");
@@ -12,28 +26,33 @@ const update = async () => {
 };
 
 (async () => {
-  log("\nBeginning remote updating process!");
   (async () => {
-    const response = await prompt({
-      type: "text",
-      name: "value",
-      message: `Running 'able update' will update your test configuration based on your 'ableConfig' file.
-  Don't forget to make changes to your your 'ableConfig' before proceeding!
-  Are you ready to update at this time?.`,
-      validate: (value) =>
-        value.toLowerCase() === "yes" ||
-        value.toLowerCase() === "no" ||
-        value.toLowerCase() === "y" ||
-        value.toLowerCase() === "n"
-          ? true
-          : "Please enter yes or no",
-    });
-
-    if (response.value === "yes" || response.value === "y") {
-      await update();
+    if (!ableConfigExists()) {
+      missingAbleConfigMessage();
       return;
     } else {
-      log("\nRemote configuration update canceled.\n");
+      log("\nBeginning remote updating process!");
+      const response = await prompt({
+        type: "text",
+        name: "value",
+        message: `Running 'able update' will update your test configuration based on your 'ableConfig' file.
+    Don't forget to make changes to your your 'ableConfig' before proceeding!
+    Are you ready to update at this time?.`,
+        validate: (value) =>
+          value.toLowerCase() === "yes" ||
+          value.toLowerCase() === "no" ||
+          value.toLowerCase() === "y" ||
+          value.toLowerCase() === "n"
+            ? true
+            : "Please enter yes or no",
+      });
+
+      if (response.value === "yes" || response.value === "y") {
+        await update();
+        return;
+      } else {
+        log("\nRemote configuration update canceled.\n");
+      }
     }
   })();
 })();
