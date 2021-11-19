151293b039116d2e746d38a6fab3b544bcf2f973
Jenae Janzen <63259035+Jjanzen27@users.noreply.github.com>
2021-11-19 12:35:35 -0500

    switch from domain pattern to domain

--- actions/addWorkerToDomain.js
+++ actions/addWorkerToDomain.js
@@ -10,13 +10,13 @@ async function addWorkerToDomain() {
   const EMAIL = process.env.EMAIL;
   const API_KEY = process.env.API_KEY;
   const ZONE_ID = process.env.ZONE_ID;
-  const DOMAIN_PATTERN = process.env.DOMAIN_PATTERN;
+  const DOMAIN = process.env.DOMAIN;
   const WORKER_SCRIPT_NAME = process.env.WORKER_SCRIPT_NAME;
 
-  log(`\nAssigning worker to ${DOMAIN_PATTERN}`);
+  log(`\nAssigning worker to ${DOMAIN}`);
 
   let body = {
-    pattern: DOMAIN_PATTERN, // *.<usersDomainFromEnv/* will cover both www. and regular with additional paths at the end (all matches)
+    pattern: `*${DOMAIN}/*`, // *.<usersDomainFromEnv/* will cover both www. and regular with additional paths at the end (all matches)
     script: WORKER_SCRIPT_NAME,
   };
 
