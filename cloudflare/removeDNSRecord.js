f5e108835e9637f49300ace2f79754e7879a1b3d
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-11-21 13:59:58 -0800

    remove dns record by record id

---
+++ cloudflare/removeDNSRecord.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const configDir = require("../utils/configDir");
+require("dotenv").config({ path: `${configDir}/.env` });
+
+async function removeDNSRecord(recordId) {
+  try {
+    const EMAIL = process.env.EMAIL;
+    const API_KEY = process.env.API_KEY;
+    const ZONE_ID = process.env.ZONE_ID;
+    const url = `https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${recordId}`
+    const headers = {
+      "X-Auth-Email": EMAIL,
+      "X-Auth-Key": API_KEY,
+      "Content-Type": "application/json",
+    }
+
+    await axios.delete(url, { headers })
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+module.exports = removeDNSRecord;
