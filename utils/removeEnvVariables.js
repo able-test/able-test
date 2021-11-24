aa7065b9baa39e812e3c09e2355a1e4826130eff
Jordan Whistler <whistler.5@osu.edu>
2021-11-24 11:49:51 -0500

    refactor removeENV

--- utils/removeEnvVariables.js
+++ utils/removeEnvVariables.js
@@ -1,24 +1,21 @@
-const configDir = require("./configDir");
 const fs = require("fs");
-const writeToEnv = require("./writeToEnv");
+const configDir = require("./configDir");
 require("dotenv").config({ path: `${configDir}/.env` });
 
-const envString = fs.readFileSync(
-  `${configDir}/.env`,
-  { encoding: "utf8" }
-);
+const envString = fs.readFileSync(`${configDir}/.env`, { encoding: "utf8" });
 
-async function removeEnvVariables(keys) { // keys is an array of strings
+function removeEnvVariables(keys) {
+  // keys is an array of strings
   let result = "";
-  const pairStrings = envString.split('\n')
-  .filter(pair => pair !== "");
-  pairStrings.forEach(pairString => {
-    const [ key, value ] = pairString.split("=");
+  const pairStrings = envString.split("\n").filter((pair) => pair !== "");
+  pairStrings.forEach((pairString) => {
+    const [key, value] = pairString.split("=");
     if (!keys.includes(key)) {
       result = result + `${key}=${value}\n`;
     }
-  })
-  await fs.writeFileSync(`${configDir}/.env`, result);
+  });
+  keys.forEach((key) => (process.env[key] = undefined));
+  fs.writeFileSync(`${configDir}/.env`, result);
 }
 
 module.exports = removeEnvVariables;
